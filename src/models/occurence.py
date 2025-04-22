1bfdb5138dabd0d629c8b0c218e90a88be77d1bc
Skiym <abramjosh13@gmail.com>
2025-04-22 12:10:47 +0800

    feat: add Device and Occurrence models

--- src/models/occurence.py
+++ src/models/occurence.py
@@ -1,10 +1,17 @@
 from typing import Annotated
 from datetime import datetime, UTC
 from fastapi import Depends, FastAPI, HTTPException, Query
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Field, Session, SQLModel, create_engine, select, DateTime
 
-class Occurence(SQLModel, table=True):
+class Device(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    deployed_latitude: float = Field(..., description="Latitude of the occurence")
+    deployed_longitude: float = Field(..., description="Longitude of the occurence")
+    deployed_at: datetime = Field(default=datetime.now(), nullable=False)
+
+class Occurrence(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     latitude: float = Field(..., description="Latitude of the occurence")
     longitude: float = Field(..., description="Longitude of the occurence")
-    created_at: datetime = datetime('now')
+    device_id: int = Field(..., description="Id of the detecting buoy", foreign_key="device.id")
+    created_at: datetime = Field(default=datetime.now(), nullable=False)
