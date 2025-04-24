8e55f5b69bf5a251c05238b4a68dfb3532779536
Skiym <abramjosh13@gmail.com>
2025-04-24 20:28:03 +0800

    chore: comments for api routes

--- src/routers/api.py
+++ src/routers/api.py
@@ -1,4 +1,17 @@
 # API endpoints 
 
-# Post occurence (latitude, longitude, device_id)
-# Get ocurrences ()
+# Post occurrence (latitude, longitude, device_id)
+
+# Get all occurrences ()
+
+# Get occurences past a certain date ()
+
+# Get occurences detected by a certain device
+
+# Get occurences detected within a surveillance group
+
+# Post buoy or register (latitued, longitued, id,)
+
+# Put buoy to keep track of last contact
+
+# Periodic sending if buoy is still alive ?
