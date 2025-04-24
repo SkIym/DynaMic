d991e9ff90a1a846671027be66a1532b64476f48
Skiym <abramjosh13@gmail.com>
2025-04-24 21:10:20 +0800

    feat: add generic post buoy function

---
+++ src/routers/buoy.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+from ..schemas.buoy import BuoyRegister
+
+router = APIRouter(
+    prefix="/buoy",
+    tags=["buoy"]
+)
+
+# Post buoy or register (latitude, longitude, id,)
+@router.post("/")
+async def register_buoy(buoy: BuoyRegister):
+    return buoy
