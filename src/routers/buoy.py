861886617682689d600d3f8eb87eabdbbf0414a6
Skiym <abramjosh13@gmail.com>
2025-04-25 14:35:07 +0800

    feat: add create buoy route

--- src/routers/buoy.py
+++ src/routers/buoy.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from schemas.buoy import BuoyRegister
 from models.buoy import Buoy
 from sqlmodel import Session
 from db import engine
+from services.survey_group import find_survey_group
 
 router = APIRouter(
     prefix="/buoy",
@@ -13,10 +14,23 @@ router = APIRouter(
 @router.post("/create")
 async def register_buoy(buoy: BuoyRegister):
     with Session(engine) as session:
-        new_buoy = Buoy(**buoy.model_dump())
 
-        lat = new_buoy.deployed_latitude
-        long = new_buoy.deployed_longitude
+        group_id = find_survey_group(
+            session=session,
+            buoy_lat=buoy.deployed_latitude,
+            buoy_long=buoy.deployed_longitude
+        )
+
+        if group_id is None:
+            raise HTTPException(
+                status_code=404,
+                detail="Buoy is not within range of any registered surveillance groups"
+            )
+
+        new_buoy = Buoy(
+            **buoy.model_dump(),
+            group_id=group_id
+        )
 
         session.add(new_buoy)
         session.commit()
