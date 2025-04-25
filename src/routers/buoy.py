6f5d4e15194f37b4d1e99f9a947faa9b9fb708d3
Skiym <abramjosh13@gmail.com>
2025-04-25 12:59:46 +0800

    feat: add buoy registering route; no group_id foreign key yet

--- src/routers/buoy.py
+++ src/routers/buoy.py
@@ -1,12 +1,28 @@
 from fastapi import APIRouter
 from schemas.buoy import BuoyRegister
+from models.buoy import Buoy
+from sqlmodel import Session
+from db import engine
 
 router = APIRouter(
     prefix="/buoy",
     tags=["buoy"]
 )
 
-# Post buoy or register (latitude, longitude, id,)
-@router.post("/")
+# Register a buoy (latitude, longitude, id,)
+@router.post("/create")
 async def register_buoy(buoy: BuoyRegister):
-    return buoy
+    with Session(engine) as session:
+        new_buoy = Buoy(**buoy.model_dump())
+
+        lat = new_buoy.deployed_latitude
+        long = new_buoy.deployed_longitude
+
+        session.add(new_buoy)
+        session.commit()
+        session.refresh(new_buoy)
+        return new_buoy
+    
+
+
+    
