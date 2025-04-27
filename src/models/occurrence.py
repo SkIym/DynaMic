1aa6bce78c06bedd1fec9881d663185e23599c84
Skiym <abramjosh13@gmail.com>
2025-04-27 15:59:05 +0800

    fix: set occurrence id as sole primary key

--- src/models/occurrence.py
+++ src/models/occurrence.py
@@ -8,7 +8,7 @@ class Occurrence(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     latitude: float = Field(..., description="Latitude of the occurence")
     longitude: float = Field(..., description="Longitude of the occurence")
-    created_at: datetime = Field(default=datetime.now(), nullable=False, primary_key=True)
+    created_at: datetime = Field(default=datetime.now(), nullable=False)
     buoy_id: int = Field(..., description="Id of the detecting buoy", foreign_key="buoy.id")
     buoy: "Buoy" = Relationship(back_populates="occurrences")
 
