fbc6d120a949865b9e02d2cd45dd1c4e08342ede
Skiym <abramjosh13@gmail.com>
2025-04-27 16:00:32 +0800

    refactor: follow uri naming convention; add get route for all occurrences

--- src/routers/occurrence.py
+++ src/routers/occurrence.py
@@ -1,31 +1,31 @@
 from fastapi import APIRouter, HTTPException
 from schemas.occurrence import OccurrenceCreate
 from models.occurrence import Occurrence
-from sqlmodel import Session
+from sqlmodel import Session, select
 from db import engine
 from services.buoy import registered_buoy
 
 router = APIRouter(
-    prefix="/occurrence",
+    prefix="/occurrences",
     tags=["occurrence"]
 )
 
 # Register an explosion (latitude, longitude, id,)
-@router.post("/create/{id}", response_model=Occurrence)
+@router.post("/{buoy_id}", response_model=Occurrence)
 async def register_explosion(
-    id: int,
+    buoy_id: int,
     occ: OccurrenceCreate):
     with Session(engine) as session:
 
-        if not registered_buoy(session, id):
+        if not registered_buoy(session, buoy_id):
             raise HTTPException(
                 status_code=400,
-                detail="Device id is not yet registered in the system"
+                detail="Device is not yet registered in the system"
             )
 
         new_occur = Occurrence(
             **occ.model_dump(),
-            buoy_id=id
+            buoy_id=buoy_id
         )
 
         session.add(new_occur)
@@ -33,6 +33,13 @@ async def register_explosion(
         session.refresh(new_occur)
         return new_occur
     
+@router.get("")
+async def get_all_occurrences():
+    with Session(engine) as session:
+        query = select(Occurrence)
+        results = session.exec(query)
+        occurrences = results.all()
+        return occurrences
 
 
     
