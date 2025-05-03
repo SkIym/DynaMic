cb093bad25002c6fc4478a2dc534209295f650a2
Skiym <abramjosh13@gmail.com>
2025-05-03 12:42:08 +0800

    feat: add schema for occurence responses

--- src/schemas/occurrence.py
+++ src/schemas/occurrence.py
@@ -3,3 +3,9 @@ from pydantic import BaseModel
 class OccurrenceCreate(BaseModel):
     latitude: float
     longitude: float
+
+class OccurrenceDisplay(BaseModel):
+    date: str
+    time: str
+    latitude: float
+    longitude: float
