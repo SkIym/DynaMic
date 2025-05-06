e4cf3b2d98a02e980c41746d18741682bb0fc3d4
Skiym <abramjosh13@gmail.com>
2025-05-06 16:59:45 +0800

    refactor(occurrences): make buoy_id part of json instead of param

--- src/schemas/occurrence.py
+++ src/schemas/occurrence.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 class OccurrenceCreate(BaseModel):
     latitude: float
     longitude: float
-
+    buoy_id: int
 class OccurrenceDisplay(BaseModel):
     date: str
     time: str
